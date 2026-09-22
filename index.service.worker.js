6c0a4da2f102c267601225044bbd72a9b53253e0
Lucas <lucaspiegas25@gmail.com>
2026-09-21 23:30:04 -0300

    Build web al día

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790043699|1893580';
+const CACHE_VERSION = '1790044201|1930139';
 /** @type {string} */
 const CACHE_PREFIX = 'Entrega Voladora-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
